55319c679f923e60d1f18b8bf8c2acd5497a17db
Johnathan Chen <johnathanchenn@outlook.com>
2018-05-08 12:29:48 -0700

    mob7 starter

---
+++ MOB7/03-Layers-&-Beziers-Challenge/03-Layers-&-Beziers-Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  03-Layers-&-Beziers-Challenge
+//
+//  Created by Johnathan Chen on 5/7/18.
+//  Copyright © 2018 Johnathan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
