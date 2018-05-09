4dcff1d301dbb6ebe3f435c874e5817bffd84429
Johnathan Chen <johnathanchenn@outlook.com>
2018-05-09 16:25:08 -0700

    some drawings

--- MOB7/02-Frame-Bounds-Challenge/02-Frame-Bounds-Challenge/ViewController.swift
+++ MOB7/02-Frame-Bounds-Challenge/02-Frame-Bounds-Challenge/ViewController.swift
@@ -12,12 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        fillScreen()
+        chessBoard()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func fillScreen() {
+        let rect = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        let fullScreen = UIView(frame: rect)
+        fullScreen.backgroundColor = UIColor.blue
+        view.addSubview(fullScreen)
+    }
+    
+    func chessBoard() {
+        let width = view.bounds.width / CGFloat(8)
+        for i in 0 ... 9 {
+            let rect = CGRect(x: i * 10, y: 20, width: width, height: width)
+            let line = UIView(frame: rect)
+            line.backgroundColor = UIColor.brown
+            view.addSubview(line)
+        }
+        
+        
+        
+        
+        
     }
 
 
